385a41ce47c659ef8724f98aeb99c75783c3c48b
weerr12 <jokerwwe0542@gmail.com>
2023-08-10 15:35:08 +0700

    ko

--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -1,3 +1,28 @@
 export const Reply = ({ userImagePath, username, replyText, likeNum }) => {
-  return <div>your code here...</div>;
+  return (
+    <div>
+      your code here...
+      <div className="d-flex gap-2 my-2 ps-5">
+        <img
+          src={userImagePath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">{username}</span>
+          <br />
+          <span>{replyText}</span>
+          <div className="d-flex align-items-center gap-1">
+            <img src="/like.svg" width={20}></img>
+            <span className="text-muted">{likeNum}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
